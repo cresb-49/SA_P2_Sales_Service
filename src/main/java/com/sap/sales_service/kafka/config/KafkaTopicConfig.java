7fa6d2e876bfae25c2dd4e334b5a35b53304b030
Benjamin <carlosbpac@gmail.com>
2025-10-18 15:54:00 -0600

    Add Kafka topic configurations for ticket sales and update paid status

--- src/main/java/com/sap/sales_service/kafka/config/KafkaTopicConfig.java
+++ src/main/java/com/sap/sales_service/kafka/config/KafkaTopicConfig.java
@@ -9,7 +9,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 class KafkaTopicConfig {
     @Bean
-    NewTopic addsCreated() {
-        return new NewTopic(TopicConstants.UPDATE_PAID_STATUS_ADD_TOPIC, 1, (short) 1);
+    NewTopic requestTicketTopic() {
+        return new NewTopic(TopicConstants.REQUEST_TICKET_SALES_TOPIC, 1, (short) 1);
+    }
+
+    @Bean
+    NewTopic responseTicketTopic() {
+        return new NewTopic(TopicConstants.RESPONSE_TICKET_SALES_TOPIC, 1, (short) 1);
+    }
+
+    @Bean
+    NewTopic updatePaidStatusAddTopic() {
+        return new NewTopic(TopicConstants.UPDATE_PAID_STATUS_SALE_TOPIC, 1, (short) 1);
     }
 }
